f8d8859cfab817659bb8a4e49eaec48ac3188c15
ProjectSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2022-07-29 23:18:22 +0400

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
-let gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-let rename = require('gulp-rename');
-let browserSync = require('browser-sync');
-let autoprefixer = require('gulp-autoprefixer');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const cssmin = require('gulp-cssmin');
+var gulp = require('gulp'),
+	sass = require('gulp-sass')(require('sass')),
+	rename = require('gulp-rename'),
+	browserSync = require('browser-sync'),
+	autoprefixer = require('gulp-autoprefixer'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	cssmin = require('gulp-cssmin'),
+	htmlmin = require('gulp-htmlmin'),
+	copyFiles = require('gulp-copy'),
+	through = require('through2');
 
 gulp.task('sass', function () {
   return gulp.src([
   		'node_modules/normalize.css/normalize.css',
-  		"docs/scss/style.scss"
+  		"src/scss/style.scss"
   	])
   	.pipe(concat('main.scss'))
 	.pipe(sass({outputStyle: 'compressed' })) //compressed-минифицирует код
@@ -21,40 +24,31 @@ gulp.task('sass', function () {
 	.pipe(gulp.dest("docs/css"))
 	.pipe(browserSync.reload({stream: true}))
 });
-/*
-gulp.task('style', function(){
-	return gulp.src([
-		'node_modules/normalize.css/normalize.css',
-		'node_modules/magnific-popup/dist/magnific-popup.css',
-		'node_modules/slick-carousel/slick/slick.css'
-	])
-	.pipe(concat('libs.min.css'))
-	.pipe(cssmin())
-	.pipe(gulp.dest('docs/css'))
-});
 
-gulp.task('script', function(){
-	return gulp.src([
-		'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-		'node_modules/slick-carousel/slick/slick.js'
-	])
-	.pipe(concat('libs.min.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('docs/js'))
-});
-*/
 gulp.task('html', function(){
-	return gulp.src('*.html')
+	return gulp.src('src/*.html')
+		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(gulp.dest('docs'))
 		.pipe(browserSync.reload({stream: true}))
 });
+
 gulp.task('js', function(){
-	return gulp.src(['docs/js/main.js'])
+	return gulp.src(['src/js/main.js'])
 		.pipe(concat('main.min.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('docs/js'))
 		.pipe(browserSync.reload({stream: true}))
 });
 
+gulp.task('copy', function(){
+	return gulp
+		.src([
+			'src/img/*.*',
+			'src/img/icon/*.*'
+		])
+		.pipe(copyFiles('docs/images/', {prefix: 2}))
+		 .pipe(verify());
+})
 
 gulp.task('browser-sync', function() {
 	browserSync.init({
@@ -65,9 +59,26 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function(){
-	gulp.watch('docs/scss/style.scss', gulp.parallel('sass'));  //строка следит за scss и запускакт параллельно плагин sass
-	gulp.watch('docs/*.html', gulp.parallel('html'));
-	gulp.watch('docs/js/main.js', gulp.parallel('js'));
+	gulp.watch('src/scss/style.scss', gulp.parallel('sass', 'html', 'js'));  //строка следит за scss и запускакт параллельно плагин sass
+	gulp.watch('src/*.html', gulp.parallel('sass', 'html', 'js'));
+	gulp.watch('src/js/main.js', gulp.parallel('sass', 'html', 'js'));
+	gulp.watch('src/img/icon/*.*', gulp.parallel('sass', 'html', 'js', 'copy'));
+	gulp.watch('src/img/*.*', gulp.parallel('sass', 'html', 'js', 'copy'));
 });
 
-gulp.task('default', gulp.parallel('sass', 'watch', 'browser-sync'))
+gulp.task('default', gulp.parallel('sass', 'html', 'js', 'copy', 'watch', 'browser-sync'))
+
+function verify() {
+	var options = { objectMode: true };
+	return through(options, write, end);
+
+	function write(file, enc, cb) {
+		console.log('file', file.path);
+		cb(null, file);
+	}
+
+	function end(cb) {
+		console.log('done');
+		cb();
+	}
+}
